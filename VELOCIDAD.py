70b24e36f21ed52b4d5b804865fe142171a73372
kathdez7 <46934923+kathdez7@users.noreply.github.com>
2019-01-22 11:04:26 -0600

    Katia Hernández, Grupo 03

---
+++ VELOCIDAD.py
@@ -0,0 +1,11 @@
+#Autor: Katia Hernández Barrera
+#Calcula velocidad promedio, dado el tiempo y la distancia
+
+
+t = int(input("inserte tiempo del viaje en hrs. enteras: "))
+
+d = int(input("Inserte la distancia recorrida en km. enteros: "))
+
+v = d/t
+
+print("Velocidad promedio:" ,v, "Km/h")
